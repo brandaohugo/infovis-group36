ad35d27361ba9a73f79a827f0048dff6bbc221a3
Dtenwolde <dljtw98@gmail.com>
2021-02-18 16:31:06 +0100

    Map now showing on web page

--- app/main/routes.py
+++ app/main/routes.py
@@ -5,8 +5,9 @@ from decimal import Decimal
 
 import pandas as pd
 import numpy as np
-# import app
-from app import models, plots, data
+
+from app import models, plots, data, map_data
+from flask import app
 from . import main
 
 
@@ -17,14 +18,12 @@ def index():
 @main.route('/map', methods=['GET'])
 def map():
 	# print(app)
-	# print(app.static_url_path)
 
 	# print(app.has_static_folder)
 	# filename = os.path.join(app.static_folder, 'data', 'us-states.json')
-	
-	data_file = open('./static/data/us-states.json')
-	data = json.load(data_file)	
-	
+	print(map_data.us_states_map)
+	data = json.load(map_data.us_states_map)
+
 	return render_template("map.html", data=data)
 
 
@@ -49,7 +48,7 @@ def bokeh():
 @main.route("/data", methods=['GET'])
 def get_data():
 	area = request.args.get("area")
-	
+
 	property_type = request.args.get("property")
 	rental_price = request.args.get("price")
 	surface_area = request.args.get("surface")
@@ -72,13 +71,13 @@ def get_data():
 	_, probabilities = models.pred_proba(model=trained_model, input_vars=query_input)
 	proba_idx = np.where(probabilities[0] == np.amax(probabilities[0]))
 	proba_idx = proba_idx[0][0]
-	
+
 	#determine the index of the predicted area within the returned probabiblities array
 	pred_area = data.area_names[proba_idx]
 	proba = probabilities[0][proba_idx]
 	proba = '%.3f' % Decimal(proba)
 
-	#determine how the prediction probability of our previously predicted area has changed 
+	#determine how the prediction probability of our previously predicted area has changed
 	#due to the change in data variables of this area
 	if area is not None:
 		proba_idx = data.area_names.index(area)
@@ -90,18 +89,18 @@ def get_data():
 		plot_area = pred_area
 
 	del probabilities
-		
+
 	if plot is not None:
 		plot_data = data.model_data.loc[data.model_data['area_name'] == plot_area]
 		plot_data = plot_data.loc[:, data.model_vars]
 		plot = plots.create_hbar(plot_area, plot_data)
-		return jsonify(prediction=pred_area, prediction_proba=proba, 
+		return jsonify(prediction=pred_area, prediction_proba=proba,
 				area_changed_proba=new_proba_prev_area, plotData=plot)
 	else:
-		return jsonify(prediction=pred_area, prediction_proba=proba, 
+		return jsonify(prediction=pred_area, prediction_proba=proba,
 				area_changed_proba=new_proba_prev_area)
 
-	
+
 
 
 @main.route('/d3', methods = ['GET'])
--- app/static/js/map.js
+++ app/static/js/map.js
@@ -1,15 +1,15 @@
 /*  This visualization was made possible by modifying code provided by:
 
-Scott Murray, Choropleth example from "Interactive Data Visualization for the Web" 
-https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html   
-		
+Scott Murray, Choropleth example from "Interactive Data Visualization for the Web"
+https://github.com/alignedleft/d3-book/blob/master/chapter_12/05_choropleth.html
+
 Malcolm Maclean, tooltips example tutorial
 http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
 
 Mike Bostock, Pie Chart Legend
 http://bl.ocks.org/mbostock/3888852  */
 
-		
+
 //Width and height of map
 var width = 960;
 var height = 500;
@@ -18,12 +18,12 @@ var height = 500;
 var projection = d3.geoAlbersUsa()
 				   .translate([width/2, height/2])    // translate to center of screen
 				   .scale([1000]);          // scale things down so see entire US
-        
+
 // Define path generator
 var path = d3.geoPath()               // path generator that will convert GeoJSON to SVG paths
 		  	 .projection(projection);  // tell path generator to use albersUsa projection
 
-		
+
 // Define linear scale for output
 var color = d3.scaleLinear()
 			  .range(["rgb(213,222,217)","rgb(69,173,168)","rgb(84,36,55)","rgb(217,91,67)"]);
@@ -35,28 +35,27 @@ var svg = d3.select("body")
 			.append("svg")
 			.attr("width", width)
 			.attr("height", height);
-        
+
 // Append Div for tooltip to SVG
 var div = d3.select("body")
-		    .append("div")   
-    		.attr("class", "tooltip")               
+		    .append("div")
+    		.attr("class", "tooltip")
     		.style("opacity", 0);
 
 // Load in my states data!
 // Load GeoJSON data and merge with states data
-// d3.json("us-states.json", function(json) {
-		
-// Bind the data to the SVG and create one path per GeoJSON feature
-// svg.selectAll("path")
-// 	.data(json.features)
-// 	.enter()
-// 	.append("path")
-// 	.attr("d", path)
-// 	.style("stroke", "#fff")
-// 	.style("stroke-width", "1")
-// });
 
-		 
+// Bind the data to the SVG and create one path per GeoJSON feature
+svg.selectAll("path")
+	.data(data.features)
+	.enter()
+	.append("path")
+	.attr("d", path)
+	.style("stroke", "#fff")
+	.style("stroke-width", "1")
+
+
+
 // // Map the cities I have lived in!
 // d3.csv("cities-lived.csv", function(data) {
 
@@ -73,28 +72,28 @@ var div = d3.select("body")
 // 	.attr("r", function(d) {
 // 		return Math.sqrt(d.years) * 4;
 // 	})
-// 		.style("fill", "rgb(217,91,67)")	
-// 		.style("opacity", 0.85)	
+// 		.style("fill", "rgb(217,91,67)")
+// 		.style("opacity", 0.85)
 
-// 	// Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks" 
+// 	// Modification of custom tooltip code provided by Malcolm Maclean, "D3 Tips and Tricks"
 // 	// http://www.d3noob.org/2013/01/adding-tooltips-to-d3js-graph.html
-// 	.on("mouseover", function(d) {      
-//     	div.transition()        
-//       	   .duration(200)      
-//            .style("opacity", .9);      
+// 	.on("mouseover", function(d) {
+//     	div.transition()
+//       	   .duration(200)
+//            .style("opacity", .9);
 //            div.text(d.place)
-//            .style("left", (d3.event.pageX) + "px")     
-//            .style("top", (d3.event.pageY - 28) + "px");    
-// 	})   
+//            .style("left", (d3.event.pageX) + "px")
+//            .style("top", (d3.event.pageY - 28) + "px");
+// 	})
 
-//     // fade out tooltip on mouse out               
-//     .on("mouseout", function(d) {       
-//         div.transition()        
-//            .duration(500)      
-//            .style("opacity", 0);   
+//     // fade out tooltip on mouse out
+//     .on("mouseout", function(d) {
+//         div.transition()
+//            .duration(500)
+//            .style("opacity", 0);
 //     });
-// });  
-        
+// });
+
 // Modified Legend Code from Mike Bostock: http://bl.ocks.org/mbostock/3888852
 // var legend = d3.select("body").append("svg")
 //       			.attr("class", "legend")
