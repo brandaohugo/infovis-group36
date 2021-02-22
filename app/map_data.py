8ef4be14be8ffd1c630b5db1a7ab33f0aca29b31
Hugo Brandao <hugo@feelrobotics.com>
2021-02-22 17:08:09 +0100

    use 2018 version of used airports

--- app/map_data.py
+++ app/map_data.py
@@ -2,4 +2,4 @@ import json
 import pandas as pd
 
 us_states_map = json.load(open('app/data/us-states.json'))
-us_airport_locations = pd.read_csv('app/data/airports.csv').to_dict('records')
+us_airport_locations = pd.read_csv('app/data/airports_2018.csv').to_dict('records') # NOTE: airports filtered to show only locations used in 2018 data.
