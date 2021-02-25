5d671509c48523052cbc5e020806d9903698d6f5
Dtenwolde <dljtw98@gmail.com>
2021-02-25 16:15:44 +0100

    renamed map.html to mainview.html

--- app/main/routes.py
+++ app/main/routes.py
@@ -25,7 +25,7 @@ def map():
     table_data = pd.pivot_table(table_data, index='MONTH', columns='AIRLINE', values='ARR_DELAY').round(2)
     table_data = table_data.to_json(orient='records')
 
-    return render_template("map.html", map_data=map_data.us_states_map, airport_locations=map_data.us_airport_locations,
+    return render_template("mainview.html", map_data=map_data.us_states_map, airport_locations=map_data.us_airport_locations,
                            plot_data=plot_data, table_data=table_data)
 
 
