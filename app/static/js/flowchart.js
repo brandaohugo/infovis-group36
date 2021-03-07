e6570bbf50fc1dd39ed33eab179e6e71e57e6432
Hugo Brandao <hugo@feelrobotics.com>
2021-03-07 10:35:50 +0100

    code style refactor

--- app/static/js/flowchart.js
+++ app/static/js/flowchart.js
@@ -57,53 +57,51 @@ const showOriginAirportFlow = (origin, flights) => {
   const nodePadding = 15;
 
   // append the svg object to the div
-  var svg = d3.select(divId).append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight)
-      .append("g")
-      .attr("transform",
-          "translate(" + margins.left + "," + margins.top + ")");
+  const svg = d3.select(divId).append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight)
+    .append("g")
+    .attr("transform",
+        "translate(" + margins.left + "," + margins.top + ")");
 
   // Color scale used
-  var color = d3.scaleOrdinal(d3.schemePastel1);
+  const color = d3.scaleOrdinal(d3.schemePastel1);
 
   // Set the sankey diagram properties
-  var sankey = d3.sankey()
-      .nodeWidth(nodeWidth)
-      .nodePadding(nodePadding)
-      .size([chartWidth, chartHeight]);
+  const sankey = d3.sankey()
+    .nodeWidth(nodeWidth)
+    .nodePadding(nodePadding)
+    .size([chartWidth, chartHeight]);
 
-  console.log(nodes)
-  console.log(links)
   sankey
-      .nodes(nodes)
-      .links(links)
-      .layout(1);
+    .nodes(nodes)
+    .links(links)
+    .layout(1);
 
   // add in the links
-  var link = svg.append("g")
-      .selectAll(".link")
-      .data(links)
-      .enter()
-      .append("path")
-      .attr("class", "link")
-      .attr("d", sankey.link())
-      .style("stroke-width", function (d) {
-          return Math.max(1, d.dy);
-      })
-      .sort(function (a, b) {
-          return b.dy - a.dy;
-      });
+  const link = svg.append("g")
+    .selectAll(".link")
+    .data(links)
+    .enter()
+    .append("path")
+    .attr("class", "link")
+    .attr("d", sankey.link())
+    .style("stroke-width", function (d) {
+        return Math.max(1, d.dy);
+    })
+    .sort(function (a, b) {
+        return b.dy - a.dy;
+    });
 
   // add in the nodes
   var node = svg.append("g")
-      .selectAll(".node")
-      .data(nodes)
-      .enter().append("g")
-      .attr("class", "node")
-      .attr("transform", function (d) {
-          return "translate(" + d.x + "," + d.y + ")";
-      })
+    .selectAll(".node")
+    .data(nodes)
+    .enter().append("g")
+    .attr("class", "node")
+    .attr("transform", function (d) {
+        return "translate(" + d.x + "," + d.y + ")";
+    })
   // .call(d3.drag()
   // .subject(function(d) { return d; })
   // .on("start", function() { this.parentNode.appendChild(this); })
@@ -112,46 +110,39 @@ const showOriginAirportFlow = (origin, flights) => {
 
   // add the rectangles for the nodes
   node
-      .append("rect")
-      .attr("height", function (d) {
-          return d.dy;
-      })
-      .attr("width", sankey.nodeWidth())
-      .style("fill", function (d) {
-          return d.color = color(d.name.replace(/ .*/, ""));
-      })
-      .style("stroke", function (d) {
-          return d3.rgb(d.color).darker(2);
-      })
-      // Add hover text
-      .append("title")
-      .text(function (d) {
-          return d.name + "\n" + "There is " + d.value + " stuff in this node";
-      });
+    .append("rect")
+    .attr("height", function (d) {
+        return d.dy;
+    })
+    .attr("width", sankey.nodeWidth())
+    .style("fill", function (d) {
+        return d.color = color(d.name.replace(/ .*/, ""));
+    })
+    .style("stroke", function (d) {
+        return d3.rgb(d.color).darker(2);
+    })
+    // Add hover text
+    .append("title")
+    .text(function (d) {
+        return d.name + "\n" + "There is " + d.value + " stuff in this node";
+    });
 
   // add in the title for the nodes
   node
-      .append("text")
-      .attr("x", -6)
-      .attr("y", function (d) {
-          return d.dy / 2;
-      })
-      .attr("dy", ".35em")
-      .attr("text-anchor", "end")
-      .attr("transform", null)
-      .text(function (d) {
-          return d.name;
-      })
-      .filter(function (d) {
-          return d.x < width / 2;
-      })
-      .attr("x", 6 + sankey.nodeWidth())
-      .attr("text-anchor", "start");
-
-
-  // load the data
-  // d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/data_sankey.json", function(error, graph) {
-
-
-  // });
+    .append("text")
+    .attr("x", -6)
+    .attr("y", function (d) {
+        return d.dy / 2;
+    })
+    .attr("dy", ".35em")
+    .attr("text-anchor", "end")
+    .attr("transform", null)
+    .text(function (d) {
+        return d.name;
+    })
+    .filter(function (d) {
+        return d.x < width / 2;
+    })
+    .attr("x", 6 + sankey.nodeWidth())
+    .attr("text-anchor", "start");
 };
