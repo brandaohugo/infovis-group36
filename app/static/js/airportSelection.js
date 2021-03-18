06e135b4316f4615041ef06f0d38e01a63a9187c
Jim <jimstam@hotmail.com>
2021-03-18 12:37:44 +0100

    Show lollipop after destination as well

--- app/static/js/airportSelection.js
+++ app/static/js/airportSelection.js
@@ -102,8 +102,8 @@ function showDestinationAirportInfo() {
     d3.select("#origin-chart")
         .style("display", "none")
 
-    d3.select("#lollipop-chart")
-        .style("display", "none")
+    // d3.select("#lollipop-chart")
+    //     .style("display", "none")
 
     d3.select("#destination-airport")
         .style("display", "block")
@@ -111,7 +111,7 @@ function showDestinationAirportInfo() {
     d3.select("#od-row")
         .style("display", "block")
 
-    // $('#lollipop-chart').insertAfter('#od-lollipop-row');
+    $('#lollipop-chart').insertAfter('#od-lollipop-row');
 }
 
 function resetToMainView() {
